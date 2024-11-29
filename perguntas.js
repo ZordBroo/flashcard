9b47d3ef032f9371eb30f0ebe9aba292357d26e6
z0rd is back <143544014+ZordBroo@users.noreply.github.com>
2024-11-29 08:15:12 -0300

    santooooos

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+
+    'Santos FC',
+    'Qual foi o último título do Santos?',
+    'Campeonato Brasileiro Serie B 2024'
+)
+
+criaCartao(
+    'Santos FC',
+    'Qual capitão do Santos que virou ídolo?',
+    'Zito'
+)
+
+criaCartao(
+    'Santos FC',
+    'Qual o nome do presidente que rebaixou o Santos?',
+    'Andrés Rueda '
+)
+
+criaCartao(
+    'Santos FC',
+    'Qual o verdaeiro noma a VIla Belmiro?',
+    'Estádio Urbano Caldeira'
+)
